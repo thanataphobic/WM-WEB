b8df0436838749d04ca10dce4e36ba14602ae129
thanataphobic <thanataphobic@user.noreply.github.com>
2023-01-08 14:46:53 -0500

    adds major updates to the site
    sidebar transition
    font changes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
- 
+ // The fade effect on the main thumbnail
  const checkpoint = 300;
  window.addEventListener("scroll", () => {
     const currentScroll = window.pageYOffset;
@@ -9,3 +9,14 @@
     }
     document.querySelector(".front").style.opacity = opacity;
  });
+
+// Width of the sidenav and margin
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+// width of the sidebar when closed
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
